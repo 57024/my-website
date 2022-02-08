097b2097f81b3d00af0ba334799a46318cf8c93b
57024 <97938139+57024@users.noreply.github.com>
2022-02-08 17:08:46 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,59 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>park jimin</td>
+        <td>jimin</td>
+        <td><img src="https://www.kindpng.com/picc/m/110-1105156_jimin-png-png-sticker-jimin-bts-bangtanboys-kpop.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+        <tr>
+        <td>2</td>
+        <td>kim taehyung</td>
+        <td>v</td>
+        <td><img src="https://www.nicepng.com/png/detail/440-4403445_bts-taehyung-v-png-by-minawastaken-bts-taehyung.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+        <tr>
+        <td>3</td>
+        <td>kim soekjin</td>
+        <td>jin</td>
+        <td><img src="https://www.pngitem.com/pimgs/m/342-3425893_pin-by-marwa-ahmadi-on-bts-png-in.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+        <tr>
+        <td>4</td>
+        <td>kim namjoon</td>
+        <td>rm</td>
+        <td><img src="https://www.kindpng.com/picc/m/345-3457489_bts-president-hot-kimnamjoon-namjoon-joonie-godofdestruction-kim.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+        <tr>
+        <td>5</td>
+        <td>min yoongi</td>
+        <td>suga</td>
+        <td><img src="https://www.kindpng.com/picc/m/744-7449008_transparent-yoongi-png-bts-png-cute-suga-png.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+        <tr>
+        <td>6</td>
+        <td>joen jungkook</td>
+        <td>jk</td>
+        <td><img src="https://www.kindpng.com/picc/m/684-6846207_jungkook-png-bts-png-jungkookpng-btspng-transparent-png.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+        <tr>
+        <td>7</td>
+        <td>jung hoesoek</td>
+        <td>jahope</td>
+        <td><img src="https://www.nicepng.com/png/detail/628-6284831_jhope-rude.png" width="100px"></td>
+        <td><a href="https://twitter.com/bts_twt?s=21">twitter</a></td>
+      </tr>
+    </table>
+  </body> 
+</html>
