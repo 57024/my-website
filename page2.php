94abc246589cf2d9ae9e414da91edfc387c11dcd
57024 <97938139+57024@users.noreply.github.com>
2022-02-15 14:17:26 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: lightblue;
+			}
+                        body{
+                                background-color: lightpink;
+			}
+                        h1{
+                                color: yellow;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1> น.ส.กฤติยาพร ไหมสกุล เลขที่ 15 ม.4/13</h1>
+		</div>
+	</body>
+</html>
