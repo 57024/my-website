50d8fac723b07645112b56e9605542884c828b73
57024 <97938139+57024@users.noreply.github.com>
2022-02-22 14:32:21 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(ChocolateteehaypaiTEST.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(Doughnut.woff)
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวกฤติยาพร ไหมสกุล ม.4/13 เลขที่ 15 เลขประจำตัว 57024</h1>
+  <h1 id="font2">นางสาวกฤติยาพร ไหมสกุล ม.4/13 เลขที่ 15 เลขประจำตัว 57024</h1>
+</body>
+</html>
